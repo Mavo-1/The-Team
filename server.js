8b5c2ca2e9dbd027e3b0b7ce96f5858ed5169db6
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-15 20:51:35 -0600

    server js and database commit

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = require('express')
 const Port = 4141
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('flash')
+const logger = require('morgan')
+const connectDB= require('./config/database')
 
 
 
