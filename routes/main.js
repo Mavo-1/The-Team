27ccfbd83a79be4ead227190ae89e0a4d4498b6f
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-21 11:47:40 -0600

    adding controllers

--- routes/main.js
+++ routes/main.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const router = express.Router()
 
-//@desc Login/Landing Page
+//@desc Landing Page
 //@route Get /
 
 router.get('/',(req,res) => {
     res.render('index')
 })
+
+//@desc Login Page
+//@route Get /login
+router.get('/login',(req,res) => {
+    res.render('login')
+})
+
+//@desc Landing Page
+//@route Get /
+
+
+//@desc Landing Page
+//@route Get /
+
+//@desc Landing Page
+//@route Get /
 
 
 
