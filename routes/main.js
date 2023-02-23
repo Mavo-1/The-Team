22817e0a72bd3f85513f858607e93f91a498db6c
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-22 20:43:04 -0600

    updating

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,12 @@ const router = express.Router()
 const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 
-//@desc Landing Page
-//@route Get /
 
 router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login',authController.postLogin)
 router.get('/logout',authController.logout)
+
 
 
 
