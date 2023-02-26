00b9a2b5178467472972874f49d57d2363ca4af1
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-26 10:43:07 -0600

    updates

--- controllers/auth.js
+++ controllers/auth.js
@@ -58,5 +58,30 @@ exports.logout = (req,res) => {
  }
 
  exports.postSignup= (req,res) => {
+  '/user',
+  // username must be an email
+  body('username').isEmail(),
+  // password must be at least 5 chars long
+  body('password').isLength({ min: 5 }),
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    User.create({
+      username: req.body.username,
+      password: req.body.password,
+    }).then(user => res.json(user));
+  },
+  {
+    "errors": [
+      {
+        "location": "body",
+        "msg": "Invalid",
+        "param": "username"
+      }
+    ]
+  }
  }
