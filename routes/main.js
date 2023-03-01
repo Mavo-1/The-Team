3b759fa421ea5b27ac57509336680b3fd4f87a53
Alejandro Garcia <agarciacoding@gmail.com>
2023-03-01 10:49:18 -0600

    update

--- routes/main.js
+++ routes/main.js
@@ -4,15 +4,12 @@ const homeController = require('../controllers/home')
 const authController = require('../controllers/auth')
 
 
-
 router.get('/', homeController.getIndex)
 router.get('/login', authController.getLogin)
 router.post('/login',authController.postLogin)
 router.get('/logout',authController.logout)
 router.get('/signup', authController.getSignup)
 router.get('/signup',authController.postSignup)
-
-
 
 
 module.exports = router
