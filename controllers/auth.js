1eb459237d602406a24f0a195f3c3377522f2282
Alejandro Garcia <agarciacoding@gmail.com>
2023-03-09 00:29:46 -0600

    TESTING

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require('../models/User')
 
 exports.getLogin = (req,res) => {
     if(req.user){
-        return res.redirect('/home')
+        return res.redirect('/index')
     }
     res.render('login', {
         title: 'Login'
