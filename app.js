695e8095100c850e29fc5b729785c46b488320b5
Alejandro Garcia <agarciacoding@gmail.com>
2023-10-18 14:12:48 -0500

    updates

--- app.js
+++ app.js
@@ -22,8 +22,10 @@ require('./config/passport')(passport)
 
 connectDB()
 
+
+
 app.engine('html',require('ejs').renderFile)
-app.set('view engine', 'html')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
