c0f6cfbdbafedf0f54cea045e4d413bd4519a66f
Alejandro Garcia <agarciacoding@gmail.com>
2023-11-08 19:01:14 -0600

    added calender updates

--- public/javascript/calendar.js
+++ public/javascript/calendar.js
@@ -1,16 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var calendarEl = document.getElementById("calendar");
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: "dayGridMonth", // You can change the initial view as needed
-      events: [
-        // Add your events here (you can use an array of event objects)
-        // Example event object:
-        // {
-        //   title: "Event Title",
-        //   start: "2023-11-15", // Date in ISO format (YYYY-MM-DD)
-        //   allDay: true, // Set to true for all-day events
-        // },
-      ],
-    });
-    calendar.render();
+  var calendarEl = document.getElementById("calendar");
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    height: "auto", // Set the height to auto to make it full size
+
+    // Add your other configuration options here
+
+    // ...
   });
+
+  calendar.render();
+});
