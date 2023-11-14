687bdab5b1475b758608494d96cb6c57800e4e19
Alejandro Garcia <agarciacoding@gmail.com>
2023-11-13 19:21:54 -0600

    updated schedule edit buttons

--- public/javascript/schedule.js
+++ public/javascript/schedule.js
@@ -1,46 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Elements
   const addGameButton = document.getElementById('addGameButton');
   const addGameModal = document.getElementById('addGameModal');
+  const closeAddGameModal = document.getElementById('closeAddGameModal');
+  const editButtons = document.querySelectorAll('[id^="editGameButton_"]');
   const editGameModal = document.getElementById('editGameModal');
-  const gameList = document.getElementById('gameList');
-
-  const showModal = (modal) => modal.classList.remove('hidden');
-  const hideModal = (modal) => modal.classList.add('hidden');
-
-  // Function to toggle the visibility of the Add Game form
-  addGameButton.addEventListener('click', () => showModal(addGameModal));
-
-  // Function to show the edit modal
-  const showEditModal = (gameId) => showModal(editGameModal);
-
-  // Add event listener for the Edit button
+  const closeEditModal = document.getElementById('closeEditModal');
+  // const gameList = document.getElementById('gameList');
  
-  gameList.addEventListener('click', (event) => {
-    if (event.target.classList.contains('edit-button')) {
-      const gameId = event.target.getAttribute('data-id');
-      showEditModal(gameId);
-    }
-  });
 
-// Save button click action
-window.updateGame = () => {
-  // Add logic to handle updating the game
-  console.log('Update Game logic goes here');
+  addGameButton.addEventListener("click", function (){
+    addGameModal.classList.remove("hidden");
+  })
 
-  // Close the edit modal after updating
-  closeEditModal();
-};
+  // Close button click action for the add game modal
+closeAddGameModal.addEventListener("click", function () {
+  addGameModal.classList.add("hidden");
+});
 
-// Close button click action
-window.closeEditModal = () => {
-  // Add logic to handle closing the edit modal
-  console.log('Close Edit Modal logic goes here');
-  hideModal(editGameModal);
-};
+//Edit button click action
 
-  // Close the "Add Game" modal
-  document.getElementById('closeGameModalButton').addEventListener('click', () => hideModal(addGameModal));
+editButtons.forEach(function (editButton) {
+  editButton.addEventListener("click", function (){
+    const gameId = editButton.getAttribute('data-id');
+    console.log('Edit button clicked for game with ID:', gameId);
+     //Add code to use gameId for any specific actions here
+     
+     //shows editGameModal
+     editGameModal.classList.remove("hidden")
+  })
+})
 
-  // Close the "Edit Game" modal
-  document.getElementById('closeEditGameModalButton').addEventListener('click', () => hideModal(editGameModal));
+
+
+closeEditModal.addEventListener("click", function () {
+  editGameModal.classList.add("hidden")
+});
+
+const submitGameButton = document.getElementById("submitGameButton")
+
+
+ submitGameButton.addEventListener("click",function (){
+
+ })
 });
