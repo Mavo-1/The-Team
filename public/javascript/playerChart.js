8a538854afa82a99b838f8cf69d69ba295f946a4
Alejandro Garcia <agarciacoding@gmail.com>
2024-01-05 02:58:10 -0600

    updates

--- public/javascript/playerChart.js
+++ public/javascript/playerChart.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1,
-                data: [10, 15, 20, 18, 25],
+                data: [123, 321, 1231, 1328, 2225],
             }],
         };
 
