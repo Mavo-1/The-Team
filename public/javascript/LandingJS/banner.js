733b51885f556e73880a9a978c7db221f72553dc
Alejandro Garcia <agarciacoding@gmail.com>
2024-02-09 03:30:00 -0600

    multiple updates to banner

---
+++ public/javascript/LandingJS/banner.js
@@ -0,0 +1,13 @@
+// Array of different sports teams
+const sportsTeams = ['soccer team', 'football team', 'baseball team'];
+let index = 0;
+
+// Function to change the text of the span element
+function changeTeamText() {
+    const spanElement = document.querySelector('#team-heading span');
+    index = (index + 1) % sportsTeams.length;
+    spanElement.textContent = sportsTeams[index];
+}
+
+// Timer to change the text every 5 seconds
+setInterval(changeTeamText, 5000);
